cf69ff10ac59ef1de1a34af67a4308b95e49092f
theroxz <ahmet.eren4219@hotmail.com>
2022-10-29 13:49:27 +0300

    Filehandler

--- src/IOHandler.java
+++ src/IOHandler.java
@@ -1,25 +1,48 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 public class IOHandler {
+    private byte[] key; //getBytes
+    private byte[] initilizationVector; //getBytes
+    private byte[] counterVal; //getBytes
     private Path inputPath;
+    private FileInputStream fkey;
     private FileOutputStream fout;
     private FileOutputStream flogger;
-    public IOHandler(String inputFile, String outputFile, String logFile){
+    public IOHandler(String inputFile, String outputFile, String logFile, String keyFile){
         try {
+            this.inputPath = Paths.get(inputFile);
             this.fout = new FileOutputStream(outputFile);
             this.flogger = new FileOutputStream(logFile);
-            this.inputPath = Paths.get(inputFile);
+            this.fkey = new FileInputStream(keyFile);
+            this.readKeyFile();
+
         }catch(Exception e){System.out.println(e);}
+    }
+    private void readKeyFile() throws IOException{
+        int i=0;
+        StringBuilder s = new StringBuilder();
+        while((i=fkey.read())!=-1){
+            char myChar = (char) i;
+            if (myChar == ' ') continue;
+            s.append(myChar);
+        }
+        String[] arr = s.toString().split("-");
+        //this.key = Base64.getDecoder().decode(arr[0].substring(0,8));
+        this.key = arr[0].substring(0,8).getBytes();
+        this.initilizationVector = arr[1].substring(0,8).getBytes();
+        this.counterVal = arr[2].substring(0,8).getBytes();
+        fkey.close();
     }
     public void writeLogFile(String msg) throws IOException {
         flogger.write(msg.getBytes());
     }
-    public byte[] readAllBytes() throws IOException{
-        return Files.readAllBytes(this.inputPath);
+    public byte[] readPlaintext() throws IOException{
+        return Files.readAllBytes(inputPath);
     }
     public void writeOutputFile(byte[] data) throws IOException{
         fout.write(data);
@@ -27,6 +50,30 @@ public class IOHandler {
     public void closeFiles() throws IOException{
         fout.close();
         flogger.close();
+    }
+
+    public byte[] getKey() {
+        return key;
+    }
+    public String getKeyAsString() throws IOException{
+        InputStream inputStream = new ByteArrayInputStream(key);
+        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    public byte[] getIV() {
+        return initilizationVector;
+    }
+    public String getIVAsString() throws IOException{
+        InputStream inputStream = new ByteArrayInputStream(initilizationVector);
+        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    public byte[] getCounterVal() {
+        return counterVal;
+    }
+    public String getCounterValAsString() throws IOException{
+        InputStream inputStream = new ByteArrayInputStream(counterVal);
+        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            IOHandler IOHandler = new IOHandler("input.txt", "output.txt", "myLogger.log");
-            byte[] data = IOHandler.readAllBytes();
+            IOHandler IOHandler = new IOHandler("input.txt", "output.txt", "myLogger.log", "keyFile.txt");
+            byte[] data = IOHandler.readPlaintext();
+            InputStream inputStream = new ByteArrayInputStream(data);
+            String myPlaintext = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            System.out.println(myPlaintext);
             IOHandler.writeOutputFile(data);
             IOHandler.writeLogFile("inputfile.txt encrypted.txt enc DES CBC 64");
+            System.out.println(IOHandler.getKeyAsString()+" "+ IOHandler.getIVAsString()+" "+ IOHandler.getCounterValAsString());
             IOHandler.closeFiles();
         }
         catch (Exception e){
+            e.printStackTrace();
             System.out.println(e);
         }
     }
